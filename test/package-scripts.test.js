c68ec37d1d2526853f0e16775d15e7a2a6b8a9ae
Hernando_Zhao <Hernando_Zhao@noreply.gitcode.com>
2026-06-02 00:45:47 +0800

    Close npm test Node wrapper risk

--- test/package-scripts.test.js
+++ test/package-scripts.test.js
@@ -10,5 +10,5 @@ test("default test scripts run through the Node18 wrapper", () => {
   const scripts = packageScripts();
 
   assert.match(scripts.test, /^node tools\/run-with-node18\.mjs --test test\/\*\.test\.js$/);
-  assert.match(scripts["test:coverage"], /^node tools\/run-with-node18\.mjs --test --experimental-test-coverage /);
+  assert.match(scripts["test:coverage"], /^node tools\/run-with-node18\.mjs --test --experimental-test-coverage .* test\/\*\.test\.js$/);
 });
